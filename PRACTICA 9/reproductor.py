ea5d4ee45fd6fa432f6fb893dc8e1f20a2bba63f
braulio sarabia <sarabiabraulio22@gmail.com>
2025-10-28 09:09:08 -0700

    PRACTICA ANTERIOR

---
+++ PRACTICA 9/reproductor.py
@@ -0,0 +1,7 @@
+playlist_musica =  ["bohemianrhapsody","hotel california","star way to heaven"]
+playlist_musica.insert(3, "pimplim")
+playlist_musica [playlist_musica.index("hotel california")]= "shape of you"
+playlist_musica.insert(0,"watermelon sugar")
+playlist_musica.pop()
+print(playlist_musica)
+print("numero de canciones:", len (playlist_musica))
